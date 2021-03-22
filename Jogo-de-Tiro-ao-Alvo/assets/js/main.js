797bdf0bc08d98c4555dc2112a4412f139ab3451
ThesllaDev <theslladev@gmail.com>
2021-03-22 12:53:56 -0300

    Feat: contagem de acerto e erros

--- Jogo-de-Tiro-ao-Alvo/assets/js/main.js
+++ Jogo-de-Tiro-ao-Alvo/assets/js/main.js
@@ -34,3 +34,21 @@ var displays = function () {
 };
 
 setInterval(displays, 1500);
+
+var hits = document.getElementById("number-of-hits");
+var numberOfHits = 0;
+var errors = document.getElementById("number-of-errors");
+var numberOfErrors = 0;
+
+canvas.onclick = function (event) {
+    var x = event.pageX - canvas.offsetLeft;
+    var y = event.pageY - canvas.offsetTop;
+    if ((x > targetPositionX - targetRadius) && (x < targetPositionX + targetRadius) &&
+        (y > targetPositionY - targetRadius) && (y < targetPositionY + targetRadius)) {
+        numberOfHits++;
+        hits.innerHTML = numberOfHits;
+    } else {
+        numberOfErrors++;
+        errors.innerHTML = numberOfErrors;
+    };
+};
