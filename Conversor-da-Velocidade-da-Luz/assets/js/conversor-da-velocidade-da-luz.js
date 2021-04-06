1cf3172447c5a1901b23f8307cb29c3c4743a517
ThesllaDev <theslladev@gmail.com>
2021-04-06 15:46:00 -0300

    Refactor: replaces the old code with new features

--- Conversor-da-Velocidade-da-Luz/assets/js/conversor-da-velocidade-da-luz.js
+++ Conversor-da-Velocidade-da-Luz/assets/js/conversor-da-velocidade-da-luz.js
@@ -1,5 +1,8 @@
 function converterValores() {
-    var quantidadeAnosLuz = document.getElementById('anosLuz').value;
-    var distanciaEmKM = (quantidadeAnosLuz * 300000 * 31536000);
-    document.getElementById('message').innerHTML = 'A distância convertida é de aproximadamente: ' + distanciaEmKM.toLocaleString('pt-BR') + '<abbr title="Quilômetros">KM</abbr>';
+    const quantidadeAnosLuz = document.querySelector('#anosLuz').value;
+    const message = document.querySelector('#message');
+    message.textContent = "";
+    let distanciaEmKM = (quantidadeAnosLuz * 300000 * 31536000);
+    message.insertAdjacentHTML('afterbegin', `A distância convertida é de aproximadamente:
+    ${distanciaEmKM.toLocaleString('pt-BR')} <abbr title="Quilômetros">KM</abbr>`);
 };
