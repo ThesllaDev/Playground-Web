dd3d55d9273b6b51c1068646e3576b5b8ed58c8e
ThesllaDev <theslladev@gmail.com>
2021-04-06 17:07:30 -0300

    Refactor: replaces the old code with new features

--- Tabela-de-Classificacao/assets/js/tabela-de-classificacao.js
+++ Tabela-de-Classificacao/assets/js/tabela-de-classificacao.js
@@ -1,207 +1,207 @@
-var atlantaHawks = {
+let atlantaHawks = {
     name: "Atlanta Hawks",
     victory: 23,
     defeats: 23,
     logo: "https://logodetimes.com/times/atlanta-hawks/logo-atlanta-hawks-escudo-256.png",
     winRate: 50
 };
-var bostonCeltics = {
+let bostonCeltics = {
     name: "Boston Celtics",
     victory: 23,
     defeats: 23,
     logo: "https://logodetimes.com/times/boston-celtics/logo-boston-celtics-256.png",
     winRate: 50
 };
-var brooklynNets = {
+let brooklynNets = {
     name: "Brooklyn Nets",
     victory: 31,
     defeats: 15,
     logo: "https://logodetimes.com/times/brooklyn-nets/logo-brooklyn-nets-256.png",
     winRate: 67
 };
-var charlotteHornets = {
+let charlotteHornets = {
     name: "Charlotte Hornets",
     victory: 23,
     defeats: 22,
     logo: "https://logodetimes.com/times/charlotte-hornets/logo-charlotte-hornets-256.png",
     winRate: 51
 };
-var chicagoBulls = {
+let chicagoBulls = {
     name: "Chicago Bulls",
     victory: 19,
     defeats: 25,
     logo: "https://logodetimes.com/times/chicago-bulls/logo-chicago-bulls-benny-256.png",
     winRate: 43
 };
-var clevelandCavaliers = {
+let clevelandCavaliers = {
     name: "Cleveland Cavaliers",
     victory: 19,
     defeats: 29,
     logo: "https://logodetimes.com/times/cleveland-cavaliers/logo-cleveland-cavaliers-escudo-256.png",
     winRate: 37
 };
-var dallasMavericks = {
+let dallasMavericks = {
     name: "Dallas Mavericks",
     victory: 23,
     defeats: 21,
     logo: "https://logodetimes.com/times/dallas-mavericks/logo-dallas-mavericks-256.png",
     winRate: 52
 };
-var denverNuggets = {
+let denverNuggets = {
     name: "Denver Nuggets",
     victory: 28,
     defeats: 18,
     logo: "https://logodetimes.com/times/denver-nuggets/logo-denver-nuggets-256.png",
     winRate: 60
 };
-var detroitPistons = {
+let detroitPistons = {
     name: "Detroit Pistons",
     victory: 12,
     defeats: 33,
     logo: "https://logodetimes.com/times/detroit-pistons/logo-detroit-pistons-256.png",
     winRate: 26
 };
-var goldenState = {
+let goldenState = {
     name: "Golden State Warriors",
     victory: 22,
     defeats: 24,
     logo: "https://logodetimes.com/times/golden-state-warriors/logo-golden-state-warriors-256.png",
     winRate: 47
 };
-var houstonRockets = {
+let houstonRockets = {
     name: "Houston Rockets",
     victory: 13,
     defeats: 32,
     logo: "https://logodetimes.com/times/houston-rockets/logo-houston-rockets-256.png",
     winRate: 28
 };
-var indianaPacers = {
+let indianaPacers = {
     name: "Indiana Pacers",
     victory: 21,
     defeats: 23,
     logo: "https://logodetimes.com/times/indiana-pacers/logo-indiana-pacers-256.png",
     winRate: 47
 };
-var laClippers = {
+let laClippers = {
     name: "Los Angeles Clippers",
     victory: 31,
     defeats: 16,
     logo: "https://logodetimes.com/times/los-angeles-clippers/logo-los-angeles-clippers-256.png",
     winRate: 66
 };
-var laLakers = {
+let laLakers = {
     name: "Los Angeles Lakers",
     victory: 30,
     defeats: 17,
     logo: "https://logodetimes.com/times/los-angeles-lakers/logo-los-angeles-lakers-256.png",
     winRate: 63
 };
-var memphisGrizzlies = {
+let memphisGrizzlies = {
     name: "Memphis Grizzlies",
     victory: 21,
     defeats: 22,
     logo: "https://logodetimes.com/times/memphis-grizzlies/logo-memphis-grizzlies-256.png",
     winRate: 48
 };
-var miamiHeat = {
+let miamiHeat = {
     name: "Miami Heat",
     victory: 22,
     defeats: 24,
     logo: "https://logodetimes.com/times/miami-heat/logo-miami-heat-256.png",
     winRate: 47
 };
-var milwaukeeBucks = {
+let milwaukeeBucks = {
     name: "Milwaukee Bucks",
     victory: 29,
     defeats: 16,
     logo: "https://logodetimes.com/times/milwaukee-bucks/logo-milwaukee-bucks-256.png",
     winRate: 64
 };
-var minnesotaTimberwolves = {
+let minnesotaTimberwolves = {
     name: "Minnesota Timberwolves",
     victory: 11,
     defeats: 35,
     logo: "https://logodetimes.com/times/minnesota-timberwolves/logo-minnesota-timberwolves-256.png",
     winRate: 23
 };
-var newOrleans = {
+let newOrleans = {
     name: "New Orleans Pelicans",
     victory: 20,
     defeats: 25,
     logo: "https://logodetimes.com/times/new-orleans-pelicans/logo-new-orleans-pelicans-256.png",
     winRate: 44
 };
-var newYork = {
+let newYork = {
     name: "New York Knicks",
     victory: 24,
     defeats: 22,
     logo: "https://logodetimes.com/times/new-york-knicks/logo-new-york-knicks-256.png",
     winRate: 52
 };
-var oklahomaCity = {
+let oklahomaCity = {
     name: "Oklahoma City Thunder",
     victory: 19,
     defeats: 26,
     logo: "https://logodetimes.com/times/oklahoma-city-thunder/logo-oklahoma-city-thunder-256.png",
     winRate: 42
 };
-var orlandoMagic = {
+let orlandoMagic = {
     name: "Orlando Magic",
     victory: 15,
     defeats: 31,
     logo: "https://logodetimes.com/times/orlando-magic/logo-orlando-magic-256.png",
     winRate: 32
 };
-var philadelphia76ers = {
+let philadelphia76ers = {
     name: "Philadelphia 76ers",
     victory: 32,
     defeats: 14,
     logo: "https://logodetimes.com/times/philadelphia-76ers/logo-philadelphia-76ers-256.png",
     winRate: 69
 };
-var phoenixSuns = {
+let phoenixSuns = {
     name: "Phoenix Suns",
     victory: 31,
     defeats: 14,
     logo: "https://logodetimes.com/times/phoenix-suns/logo-phoenix-suns-256.png",
     winRate: 68
 };
-var portlandBlazers = {
+let portlandBlazers = {
     name: "Portland Trail Blazers",
     victory: 28,
     defeats: 18,
     logo: "https://logodetimes.com/times/portland-trail-blazers/logo-portland-trail-blazers-256.png",
     winRate: 60
 };
-var sacramentoKings = {
+let sacramentoKings = {
     name: "Sacramento Kings",
     victory: 21,
     defeats: 25,
     logo: "https://logodetimes.com/times/sacramento-kings/logo-sacramento-kings-256.png",
     winRate: 45
 };
-var sanAntonio = {
+let sanAntonio = {
     name: "San Antonio Spurs",
     victory: 23,
     defeats: 20,
     logo: "https://logodetimes.com/times/san-antonio-spurs/logo-san-antonio-spurs-256.png",
     winRate: 53
 };
-var torontoRaptors = {
+let torontoRaptors = {
     name: "Toronto Raptors",
     victory: 18,
     defeats: 28,
     logo: "https://logodetimes.com/times/toronto-raptors/logo-toronto-raptors-256.png",
     winRate: 39
 };
-var utahJazz = {
+let utahJazz = {
     name: "Utah Jazz",
     victory: 34,
     defeats: 11,
     logo: "https://logodetimes.com/times/utah-jazz/logo-utah-jazz-256.png",
     winRate: 75
 };
-var washingtonWizards = {
+let washingtonWizards = {
     name: "Washington Wizards",
     victory: 16,
     defeats: 28,
@@ -209,7 +209,7 @@ var washingtonWizards = {
     winRate: 36
 };
 
-var teams = [atlantaHawks,
+let teams = [atlantaHawks,
     bostonCeltics,
     brooklynNets,
     charlotteHornets,
