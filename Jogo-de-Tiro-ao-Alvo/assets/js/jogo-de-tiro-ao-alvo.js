da25b0166d7d71ff4d2f1e92acca4cf39e681b17
ThesllaDev <theslladev@gmail.com>
2021-04-06 17:04:20 -0300

    Refactor: replaces the old code with new features

--- Jogo-de-Tiro-ao-Alvo/assets/js/jogo-de-tiro-ao-alvo.js
+++ Jogo-de-Tiro-ao-Alvo/assets/js/jogo-de-tiro-ao-alvo.js
@@ -4,29 +4,29 @@ let targetPositionY;
 const canvas = document.querySelector('#canvas');
 const context = canvas.getContext('2d');
 
-var drawTarget = function (x, y) {
+let drawTarget = function (x, y) {
     circles(x, y, targetRadius + 30, 'white');
     circles(x, y, targetRadius + 20, 'red');
     circles(x, y, targetRadius + 10, 'white');
     circles(x, y, targetRadius, 'red');
 };
 
-var circles = function (x, y, targetRadius, color) {
+let circles = function (x, y, targetRadius, color) {
     context.fillStyle = color;
     context.beginPath();
     context.arc(x, y, targetRadius, 0, 2 * Math.PI);
     context.fill();
 };
 
-var clean = function () {
+let clean = function () {
     context.clearRect(0, 0, 900, 600);
 };
 
-var raffle = function (max) {
+let raffle = function (max) {
     return Math.floor(Math.random() * max);
 };
 
-var displays = function () {
+let displays = function () {
     clean();
     targetPositionX = raffle(900);
     targetPositionY = raffle(600);
@@ -41,8 +41,8 @@ const errors = document.querySelector('#number-of-errors');
 let numberOfErrors = 0;
 
 canvas.onclick = function (event) {
-    var x = event.pageX - canvas.offsetLeft;
-    var y = event.pageY - canvas.offsetTop;
+    let x = event.pageX - canvas.offsetLeft;
+    let y = event.pageY - canvas.offsetTop;
     if ((x > targetPositionX - targetRadius) && (x < targetPositionX + targetRadius) &&
         (y > targetPositionY - targetRadius) && (y < targetPositionY + targetRadius)) {
         numberOfHits++;
