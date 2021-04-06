c963923f5a44bfa899997b103ea439f619bf7c75
ThesllaDev <theslladev@gmail.com>
2021-04-06 15:36:06 -0300

    Refactor: replaces the old code with new features

--- Calculadora-de-IMC/assets/js/calculadora-imc.js
+++ Calculadora-de-IMC/assets/js/calculadora-imc.js
@@ -1,34 +1,32 @@
+
 function calcularIMC() {
-	var alturaDoUsuario = document
-		.getElementById("altura")
-		.value.replace(/,/g, ".");
-	var pesoDoUsuario = document.getElementById("peso").value.replace(/,/g, ".");
+	const alturaDoUsuario = document.querySelector('#altura').value.replace(/,/g, ".");
+	const pesoDoUsuario = document.querySelector('#peso').value.replace(/,/g, ".");
+	const message = document.querySelector('#message');
 
 	if (pesoDoUsuario == "" || alturaDoUsuario == "") {
-		document.getElementById("message").innerHTML = "Preencha os campos";
+		message.textContent = "Preencha os campos";
 		return false;
 	}
 
 	var imcDoUsuario = pesoDoUsuario / (alturaDoUsuario * alturaDoUsuario);
-	var message = "";
 
 	if (imcDoUsuario < 17) {
-		message = "Você está muito abaixo do peso";
+		message.textContent = "Você está muito abaixo do peso";
 	} else if (imcDoUsuario >= 17 && imcDoUsuario <= 18.49) {
-		message = "Você está abaixo do peso";
+		message.textContent = "Você está abaixo do peso";
 	} else if (imcDoUsuario >= 18.5 && imcDoUsuario <= 24.99) {
-		message = "Seu peso está normal";
+		message.textContent = "Seu peso está normal";
 	} else if (imcDoUsuario >= 25 && imcDoUsuario <= 29.99) {
-		message = "Você está acima do peso";
+		message.textContent = "Você está acima do peso";
 	} else if (imcDoUsuario >= 30 && imcDoUsuario <= 34.99) {
-		message = "Você tem obesidade nível I";
+		message.textContent = "Você tem obesidade nível I";
 	} else if (imcDoUsuario >= 35 && imcDoUsuario <= 39.99) {
-		message = "Você tem Obesidade nível II";
+		message.textContent = "Você tem Obesidade nível II";
 	} else if (imcDoUsuario >= 40) {
-		message = "Você tem Obesidade nível III";
+		message.textContent = "Você tem Obesidade nível III";
 	}
 
 	imcDoUsuario = imcDoUsuario.toFixed(2).replace(/\./g, ",");
-	document.getElementById("message").innerHTML =
-		"Seu IMC é : " + imcDoUsuario + "<br>" + message;
+	message.insertAdjacentHTML('afterbegin', `Seu IMC é : ${imcDoUsuario} </br>`);
 }
