72556e265b2fc8bd68f801ce1cc6399548e39cd1
ThesllaDev <theslladev@gmail.com>
2021-06-15 11:12:28 -0300

    Style: structure and formatting for readability

--- Lista-de-Filmes/assets/js/lista-de-filmes.js
+++ Lista-de-Filmes/assets/js/lista-de-filmes.js
@@ -1,6 +1,39 @@
-let moviesName = ["2 Filhos de Francisco: A História de Zezé di Camargo & Luciano", "À Espera De Um Milagre", "Cidade de Deus", "Meu Nome Não É Johnny", "Moonlight: Sob a Luz do Luar", "O Auto da Compadecida", "O Homem que Copiava", "O Poderoso Chefão", "Tropa de Elite", "Tropa de Elite 2: O Inimigo Agora é Outro"];
-let moviesPosters = ["https://www.themoviedb.org/t/p/w600_and_h900_bestv2/nopSuvnKUMZnybxxg9VmZGPJM27.jpg", "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/iye7Fwd0puLTPwxsuHrL1YczeGu.jpg", "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/pA70WUs7KHiHltfiBN4XEELOXcS.jpg", "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/9G1voavqsLFx0k66PXw11AUUaj.jpg", "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/AekOkoT88EhDHikUQXQcKri2q4B.jpg", "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/m8eFedsS7vQCZCS8WGp5n1bVD0q.jpg", "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/1wLs0uKBrJEfex534aX2dat7D5U.jpg", "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/oJagOzBu9Rdd9BrciseCm3U3MCU.jpg", "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/eYTZoNEWbHPD2ujjdhdlpvbSYdr.jpg", "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/8G7F938AHqBBLc5yJXPUJhSJzN5.jpg"]
-let trailers = ["https://www.youtube.com/watch?v=aPrDVKf8u-A", "https://www.youtube.com/watch?v=Ulv7IKP1AbI", "https://www.youtube.com/watch?v=nBWtTrLxUjM", "https://www.youtube.com/watch?v=0uFXVu3lV_c", "https://www.youtube.com/watch?v=cYFIBxizOW0", "https://www.youtube.com/watch?v=XPuMu_ENzlg", "https://www.youtube.com/watch?v=-HILhhhGb9w", "https://www.youtube.com/watch?v=y_-YWEot_7w", "https://www.youtube.com/watch?v=uZBiNJQxtGw", "https://www.youtube.com/watch?v=5tG4i8elT3I"]
+const moviesName = [
+    "2 Filhos de Francisco: A História de Zezé di Camargo & Luciano",
+    "À Espera De Um Milagre",
+    "Cidade de Deus",
+    "Meu Nome Não É Johnny",
+    "Moonlight: Sob a Luz do Luar",
+    "O Auto da Compadecida",
+    "O Homem que Copiava",
+    "O Poderoso Chefão",
+    "Tropa de Elite",
+    "Tropa de Elite 2: O Inimigo Agora é Outro"
+];
+const moviesPosters = [
+    "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/nopSuvnKUMZnybxxg9VmZGPJM27.jpg",
+    "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/iye7Fwd0puLTPwxsuHrL1YczeGu.jpg",
+    "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/pA70WUs7KHiHltfiBN4XEELOXcS.jpg",
+    "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/9G1voavqsLFx0k66PXw11AUUaj.jpg",
+    "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/AekOkoT88EhDHikUQXQcKri2q4B.jpg",
+    "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/m8eFedsS7vQCZCS8WGp5n1bVD0q.jpg",
+    "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/1wLs0uKBrJEfex534aX2dat7D5U.jpg",
+    "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/oJagOzBu9Rdd9BrciseCm3U3MCU.jpg",
+    "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/eYTZoNEWbHPD2ujjdhdlpvbSYdr.jpg",
+    "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/8G7F938AHqBBLc5yJXPUJhSJzN5.jpg"
+];
+const trailers = [
+    "https://www.youtube.com/watch?v=aPrDVKf8u-A",
+    "https://www.youtube.com/watch?v=Ulv7IKP1AbI",
+    "https://www.youtube.com/watch?v=nBWtTrLxUjM",
+    "https://www.youtube.com/watch?v=0uFXVu3lV_c",
+    "https://www.youtube.com/watch?v=cYFIBxizOW0",
+    "https://www.youtube.com/watch?v=XPuMu_ENzlg",
+    "https://www.youtube.com/watch?v=-HILhhhGb9w",
+    "https://www.youtube.com/watch?v=y_-YWEot_7w",
+    "https://www.youtube.com/watch?v=uZBiNJQxtGw",
+    "https://www.youtube.com/watch?v=5tG4i8elT3I"
+];
 const listOfFilms = document.querySelector('#listOfFilms');
 
 for (let i = 0; i < moviesName.length; i++) {
